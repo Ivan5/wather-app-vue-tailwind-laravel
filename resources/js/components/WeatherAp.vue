30c19f9e4d9cc937887e5512aaffad20d61c7437
Ivan5 <converse_ivan@hotmail.com>
2020-02-20 16:54:36 -0600

    Add vue and make the principal component

---
+++ resources/js/components/WeatherAp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-white mb-8">
+    <div class="places-input text-gray-800">
+      <input type="text" class="w-full" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
