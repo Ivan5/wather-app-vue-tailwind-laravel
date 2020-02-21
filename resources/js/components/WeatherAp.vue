1a3c9bbf2ae24a8e0da30b9172038a2cff050f24
Ivan5 <converse_ivan@hotmail.com>
2020-02-21 11:43:16 -0600

    Desing the current weather section

--- resources/js/components/WeatherAp.vue
+++ resources/js/components/WeatherAp.vue
@@ -3,6 +3,23 @@
     <div class="places-input text-gray-800">
       <input type="text" class="w-full" />
     </div>
+    <div
+      class="weather-container font-sans w-128 max-w-lg rounded-lg overflow-hidden bg-gray-900 shadow-lg mt-4"
+    >
+      <div class="current-weather flex items-center justify-between px-6 py-8">
+        <div class="flex items-center">
+          <div>
+            <div class="text-6xl font-semibold">8°C</div>
+            <div>Feels like 2°</div>
+          </div>
+          <div class="mx-5">
+            <div class="font-semibold">Cloudy</div>
+            <div>Toronto, Canada</div>
+          </div>
+        </div>
+        <div>icon</div>
+      </div>
+    </div>
   </div>
 </template>
 
